874d329694c53e4e8184502120c9af4c076f9a6c
gypceros-head <higuchi.jobseeker@gmail.com>
2025-12-04 15:38:20 +0900

    ヘッダーフッター仮ロゴ実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_css_class(key)
+    case key.to_sym
+    when :notice
+      "bg-emerald-500 text-white"
+    when :alert
+      "bg-red-500 text-white"
+    else
+      "bg-slate-700 text-white"
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module App
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.generators.template_engine = :slim #slimに変更
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
