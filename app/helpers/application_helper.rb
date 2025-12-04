7c8831aac98e5507fe423bd9ac66f99d050e6bb1
gypceros-head <higuchi.jobseeker@gmail.com>
2025-12-04 17:20:50 +0900

    フラッシュメッセージのTailwind化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def flash_css_class(key)
+  def flash_classes(key)
     case key.to_sym
     when :notice
-      "bg-emerald-500 text-white"
+      "bg-emerald-50 border-emerald-500 text-emerald-800"
     when :alert
-      "bg-red-500 text-white"
+      "bg-red-50 border-red-500 text-red-800"
     else
-      "bg-slate-700 text-white"
+      "bg-slate-50 border-slate-500 text-slate-800"
     end
   end
 end
