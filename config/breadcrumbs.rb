b4120b12a4641ea00b444116eb09659663dba335
gypceros-head <higuchi.jobseeker@gmail.com>
2025-12-17 16:25:39 +0900

    規約追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,6 +2,16 @@ crumb :root do
   link "ホーム", root_path
 end
 
+crumb :terms do
+  link "利用規約", terms_path
+  parent :root
+end
+
+crumb :privacy do
+  link "プライバシーポリシー", privacy_path
+  parent :root
+end
+
 crumb :posts do
   link "投稿一覧", posts_path
   parent :root
